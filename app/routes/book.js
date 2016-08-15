f85fe39843cbb38713e0942cca24b06fea0a3f8f
Yoseph Anderson <yrganderson@gmail.com>
2016-08-14 20:55:23 -0600

    removed comments * cleaned unnecessary comments

--- app/routes/book.js
+++ app/routes/book.js
@@ -5,11 +5,9 @@ let Book = require('../models/book');
  * GET /book route to retrieve all the books.
  */
 function getBooks(req, res) {
-    //Query the DB and if no errors, send all the books
     let query = Book.find({});
     query.exec((err, books) => {
         if(err) res.send(err);
-        //If no errors, send them back to the client
         res.json(books);
     });
 }
@@ -18,14 +16,12 @@ function getBooks(req, res) {
  * POST /book to save a new book.
  */
 function postBook(req, res) {
-    //Creates a new book
     var newBook = new Book(req.body);
-    //Save it into the DB.
     newBook.save((err,book) => {
         if(err) {
             res.send(err);
         }
-        else { //If no errors, send it back to the client
+        else {
             res.json({message: "Book successfully added!", book });
         }
     });
@@ -37,7 +33,7 @@ function postBook(req, res) {
 function getBook(req, res) {
     Book.findById(req.params.id, (err, book) => {
         if(err) res.send(err);
-        //If no errors, send it back to the client
+      
         res.json(book);
     });
 }
