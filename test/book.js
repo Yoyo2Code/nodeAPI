8dae6d86c144fb0fd173bdc34994325afb61af87
Yoseph Anderson <yrganderson@gmail.com>
2016-08-14 21:00:33 -0600

    get route api test

--- test/book.js
+++ test/book.js
@@ -15,3 +15,18 @@ describe('Books', () => {
            done();
         });
     });
+
+describe('/GET book', () => {
+      it('it should GET all the books', (done) => {
+        chai.request(server)
+            .get('/book')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(0);
+              done();
+            });
+      });
+  });
+
+});
